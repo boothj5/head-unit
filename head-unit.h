f17acb76ede442e6ff858a7c6a3c704b8ccf8d47
James Booth <boothj5@gmail.com>
2011-08-15 22:12:04 +0100

    Initial commit

---
+++ head-unit.h
@@ -0,0 +1,15 @@
+#ifndef HEAD_UNIT_H
+#define HEAD_UNIT_H
+
+#define MAX_NAME_LEN 50
+
+typedef struct {
+    void (*test)(void) ;
+    char name[MAX_NAME_LEN] ;
+} Test ;
+
+Test make_test(void (*test)(void), char *name) ;
+void assert_true(int expression) ;
+void run_tests(Test *tests, int ntests) ;
+
+#endif
---
+++ makefile
@@ -0,0 +1,14 @@
+CC=gcc
+
+make-headunit: 
+	$(CC) -o libheadunit.a -c head-unit.c
+
+install: make-headunit
+	cp libheadunit.a ~/lib
+	cp head-unit.h ~/include
+
+clean:
+	rm -f *.o
+	rm -f *.a
+	rm -f ~/lib/libheadunit.a
+	rm -f ~/include/head-unit.h	
