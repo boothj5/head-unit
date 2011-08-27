fc36c2cfe32b11db562a596f4fb968436cd7a2be
James Booth <boothj5@gmail.com>
2011-08-27 01:54:02 +0100

    Renamed constant to avoid clashes

--- head-unit.h
+++ head-unit.h
@@ -1,13 +1,12 @@
 #ifndef HEAD_UNIT_H
 #define HEAD_UNIT_H
 
-#define MAX_NAME_LEN 50
+#define MAX_TEST_NAME_LEN 50
 #define MAX_TESTS 100
-#define END_TEST "END_TEST"
 
 typedef struct {
     void (*test)(void) ;
-    char name[MAX_NAME_LEN] ;
+    char name[MAX_TEST_NAME_LEN] ;
 } Test ;
 
 void add_test(void (*test)(void), char *name) ;
