c29b7ae79de3844debf86182487990744f4b64c5
James Booth <boothj5@gmail.com>
2012-02-20 20:22:36 +0000

    Added GPL

--- head-unit.h
+++ head-unit.h
@@ -1,3 +1,25 @@
+/* 
+ * head-unit.h
+ *
+ * Copyright (C) 2011, 2012 James Booth <boothj5@gmail.com>
+ * 
+ * This file is part of head-unit.
+ *
+ * head-unit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * head-unit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with head-unit.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef HEAD_UNIT_H
 #define HEAD_UNIT_H
 
